94fd5a8f911a88b5a77a42980ce0fe3d86089ed3
Michal Musialik <michal@michalmusialik.com>
2014-02-26 16:40:38 +0100

    Add page.php

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<section class="wrapper white">
+
+  <section class="content">
+
+    <?php get_template_part( 'partials/_loop', 'single' ); ?>
+
+  </section>
+
+  <?php get_sidebar(); ?>
+    
+</section>
+
+<?php get_footer(); ?> 
--- partials/_loop-single.php
+++ partials/_loop-single.php
@@ -4,15 +4,13 @@
     
     <article class="entry">
 
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+      <h1><?php the_title() ?></h1>
 
       <?php if ( has_post_thumbnail() ) : ?>
         <?php the_post_thumbnail( 'img-single', array( 'class' => 'single-thumbnail' ) ); ?>
       <?php endif; ?>
 
-      <!-- <img src="http://lorempixel.com/220/145/cats/5" alt="" class="index-thumbnail"> -->
-
-      <?php the_content( 'Czytaj dalej...' ); ?>
+      <?php the_content(); ?>
 
       <div class="meta">Dodano: <?php echo get_the_date('j F Y'); ?>, kategoria: <?php the_category( ', ' ); ?></div>
 
