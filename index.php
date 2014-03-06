c336bfa7a2c8b5fa4f1dfb151ddd05d4dcd8cf18
Michal Musialik <michal@michalmusialik.com>
2014-03-06 12:01:53 +0100

    Reorganise scss partials

--- index.php
+++ index.php
@@ -2,14 +2,42 @@
 
 <section class="wrapper white">
 
-  <section class="content">
+	<section class="content">
 
-    <?php get_template_part( 'partials/_loop' ); ?>
+		<?php if ( have_posts() ) : ?>
+	
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+				<article class="entry">
 
-  </section>
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
 
-  <?php get_sidebar(); ?>
-    
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'img-small', array( 'class' => 'index-thumbnail' ) ); ?>
+					<?php endif; ?>
+
+					<!-- <img src="http://lorempixel.com/220/145/cats/5" alt="" class="index-thumbnail"> -->
+
+					<?php the_content( 'Czytaj dalej...' ); ?>
+
+					<div class="meta">Dodano: <?php echo get_the_date('j F Y'); ?>, kategoria: <?php the_category( ', ' ); ?></div>
+
+				</article>
+
+			<?php endwhile; ?>
+			
+			<?php get_template_part( 'partials/_pagination' ); ?>
+
+		<?php else : ?>
+
+			Nic nie znaleziono
+
+		<?php endif; ?>
+
+	</section>
+
+	<?php get_sidebar(); ?>
+		
 </section>
 
 <?php get_footer(); ?> 
