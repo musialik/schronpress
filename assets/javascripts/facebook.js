21e8e8e418dccf04f90bf4872cc5a006992de9b0
Michal Musialik <michal@michalmusialik.com>
2014-08-09 16:34:57 +0200

    Finish adding GA tracking to fb events

--- assets/javascripts/facebook.js
+++ assets/javascripts/facebook.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($) {
     FB.init({
       appId   : '317736431736355',
       xfbml   : true,
+      cookie  : true,
       version : 'v2.0'
     });     
     
@@ -11,19 +12,11 @@ jQuery(document).ready(function($) {
 
     // Google Analytics events are defined here
     FB.Event.subscribe('edge.create', function(targetUrl) {
-      console.log(targetUrl);
       _gaq.push(['_trackSocial', 'facebook', 'like', targetUrl]);
     });
 
     FB.Event.subscribe('edge.remove', function(targetUrl) {
-      console.log(targetUrl);
       _gaq.push(['_trackSocial', 'facebook', 'unlike', targetUrl]);
-    });
-
-    // FB share
-    FB.Event.subscribe('message.send', function(targetUrl) {
-      console.log(targetUrl);
-      _gaq.push(['_trackSocial', 'facebook', 'share', targetUrl]);
     });
   });
 });
