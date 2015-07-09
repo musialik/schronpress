c5c5f54b90e973a1b7c9a504f9407468c6288070
Michal Musialik <michal@michalmusialik.com>
2015-07-09 21:56:28 +0200

    Add realconstruct banner

--- footer.php
+++ footer.php
@@ -1,28 +1,34 @@
-		</div> <!-- .content-bg -->
+    </div> <!-- .content-bg -->
 
-		<section class="footer-bg">
-			<footer class="wrapper footer">
+    <section class="banners">
+      <a class='banner' title='real construct' href="http://www.realconstruct.com.pl/">
+        <img src="<?php echo get_bloginfo('template_url'); ?>/assets/images/real-construct.gif">
+      </a>
+    </section>
 
-				<aside class="footer-widget footer-left">
-					<h4 class="footer-header">Szukaj</h4>
-					<section class="footer-content">
-						<p>Przeszukaj wszystkie podstrony, aktualności i zwierzęta.</p>
-						<form action="/" method="GET">
-							<input type="search" name="s" class="search-footer">
-							<input type="submit" class="btn-footer" value="Szukaj">
-						</form>
-					</section>
-				</aside>
+    <section class="footer-bg">
+      <footer class="wrapper footer">
 
-				<aside class="footer-widget footer-right">
-					<h4 class="footer-header">Facebook</h4>
-					<p class="footer-content">Już ponad 5000 osób wspiera nas na facebooku! Dziękujemy.</p>
-					<div class="fb-like-box" data-href="https://www.facebook.com/otozoswiecim" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="false" data-show-border="false"></div>
-				</aside>
+        <aside class="footer-widget footer-left">
+          <h4 class="footer-header">Szukaj</h4>
+          <section class="footer-content">
+            <p>Przeszukaj wszystkie podstrony, aktualności i zwierzęta.</p>
+            <form action="/" method="GET">
+              <input type="search" name="s" class="search-footer">
+              <input type="submit" class="btn-footer" value="Szukaj">
+            </form>
+          </section>
+        </aside>
 
-			</footer>
-		</section>
+        <aside class="footer-widget footer-right">
+          <h4 class="footer-header">Facebook</h4>
+          <p class="footer-content">Już ponad 5000 osób wspiera nas na facebooku! Dziękujemy.</p>
+          <div class="fb-like-box" data-href="https://www.facebook.com/otozoswiecim" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="false" data-show-border="false"></div>
+        </aside>
 
-	<?php wp_footer(); ?>
-	</body>
+      </footer>
+    </section>
+
+  <?php wp_footer(); ?>
+  </body>
 </html>
